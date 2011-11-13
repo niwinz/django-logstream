e245327190f965849f345c2785151312ba1b662a
Andrei Antoukh <niwi@niwi.be>
2011-11-13 19:42:54 +0100

    Add doc to the project.

--- django_logstream/client/handlers/threaded.py
+++ django_logstream/client/handlers/threaded.py
@@ -1,29 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django_logstream.utils import Singleton
-from django_logstream.client.handlers.common import \
-    InfiniteRotatingFileHandler as CommonIRFH
 
 from Queue import Queue
 from thread import start_new_thread
 from threading import Lock
 from logging import StreamHandler
 import zmq
-
-
-class InfiniteRotatingFileHandler(CommonIRFH):
-    _queue = Queue()
-    def __init__(self, *args, **kwargs):
-        super(InfiniteRotatingFileHandler, self).__init__(*args, **kwargs)
-        start_new_thread(self._loop)
-
-    def _loop(self):
-        while True:
-            record = self._queue.get(True)
-            super(InfiniteRotatingFileHandler, self).emit(record)
-
-    def emit(self, record):
-        self._queue.put_nowait(record)
 
 
 class ZeroMQHandler(StreamHandler):
---
+++ doc/source/index.rst
@@ -0,0 +1,20 @@
+.. django-logstream documentation master file, created by
+   sphinx-quickstart on Sun Nov 13 19:41:46 2011.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to django-logstream's documentation!
+============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
